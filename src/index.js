33e603b6555dda23db31290a2953e2ef87215e07
Yuri Brunetto <brunettoyuri@gmail.com>
2019-11-23 17:08:19 -0300

    add prettier config file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 import FancyRoute from './components/common/FancyRoute'
@@ -7,18 +7,36 @@ import routes from './routes'
 
 import GlobalStyle from './GlobalStyle'
 
-ReactDOM.render(
-  <>
-    <Router>
-      <Switch>
-        {routes.map((route, i) => (
-          <FancyRoute key={i} {...route} />
-        ))}
-      </Switch>
-    </Router>
-    <GlobalStyle />
-  </>,
-  document.getElementById('root'),
-)
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <>
+      <Router>
+        <Switch>
+          {routes.map((route, i) => (
+            <FancyRoute key={i} {...route} />
+          ))}
+        </Switch>
+      </Router>
+      <GlobalStyle />
+    </>,
+    rootElement
+  )
+} else {
+  render(
+    <>
+      <Router>
+        <Switch>
+          {routes.map((route, i) => (
+            <FancyRoute key={i} {...route} />
+          ))}
+        </Switch>
+      </Router>
+      <GlobalStyle />
+    </>,
+    rootElement
+  )
+}
 
 // serviceWorker.unregister()
