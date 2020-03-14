7cd1c9e8ffceedb9d8abeee6ada65b275a4f4d3c
Yuri Brunetto <brunettoyuri@gmail.com>
2020-03-13 21:55:24 -0300

    update socials and curriculum

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -25,7 +25,11 @@ const GlobalStyle = createGlobalStyle`
   body {
     background-color: var(--dark-color);
     color: #fff;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
     -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 
     strong {
       font-weight: bold;
